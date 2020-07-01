0d72cd7ec5bf1a1b035002413a678da1fecef175
auroville <auroville.pms@gmail.com>
2020-07-01 10:39:15 +0900

    Add : Header 추가

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
+import Hearder from "Components/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Hearder />
         <Router />
       </div>
     );
