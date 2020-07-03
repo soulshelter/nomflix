787db2f0a26cad02a44e69284c4bbaf37d4a459b
auroville <auroville.pms@gmail.com>
2020-07-03 16:54:29 +0900

    Add : Axios API & API.js

---
+++ src/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "21640f22d822c1d16dc861b12baa651c",
+    language: "en-US",
+  },
+});
+
+export const tvApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get(`search/tv`, {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
+
+export const moviesApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  movieDetail: (id) =>
+    api.get(`moive/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get(`search/movie`, {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
