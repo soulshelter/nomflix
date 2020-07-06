9f1d132344d219fc4f3e7bc1896d11d885ad178d
auroville <auroville.pms@gmail.com>
2020-07-06 14:35:19 +0900

    Add : HomeContainer.js 작성
    api 연동 후 setState 구현

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { movieApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -9,6 +10,33 @@ export default class extends React.Component {
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await movieApi.nowPlaying();
+      const {
+        data: { results: upcoming },
+      } = await movieApi.upcoming();
+      const {
+        data: { results: popular },
+      } = await movieApi.popular();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find movies information.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
 
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
